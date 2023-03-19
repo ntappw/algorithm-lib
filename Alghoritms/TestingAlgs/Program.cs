be411201f4d368d532c8c968c0e73dd542c2f937
ntapp <maybeeonce@gmail.com>
2023-03-19 19:19:25 +0300

    Not important changes

--- Alghoritms/TestingAlgs/Program.cs
+++ Alghoritms/TestingAlgs/Program.cs
@@ -1,8 +1,19 @@
-﻿using Alghoritms.Classes;
+﻿using System.Data;
+using Alghoritms.Classes;
 
-int[] array = new int[] { 1, 3, 1, 2, 3, 9, 0, 23};
-// var result = Sorts.SortByCounting(array);
+// int[] a = new int[] { 1, 3, 5, 9, 12, 16 };
+// int[] b = new int[] { 0, 2, 3, 9, 24, 25 };
 //
-// if (result != null)
-//     foreach (var item in result)
-//         Console.Write(item + " | ");
+//
+// int[] array = new int[] { 1, 3, 1, 2, 3, 9, 0, 23};
+// var result = Sorts.MergeSort(array);
+// PrintArr(result);
+
+
+
+void PrintArr(int[] array)
+{
+    foreach (var item in array)
+        Console.Write(item + " | ");
+    Console.WriteLine();
+} 
